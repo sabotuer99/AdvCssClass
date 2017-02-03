1f25b0433b2a5aed5159e2187c4058114bbdcd3f
sabotuer99 <sabotuer99@gmail.com>
2017-02-03 09:47:44 -0700

    automated scss precompile with gulp

---
+++ Mod3Lab/gulpfile.js
@@ -0,0 +1,16 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(function(f) {
+            //console.log(f.base);
+            return "c:\\Projects\\AdvancedCss\\Mod3Lab\\css\\";
+        }))
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('scss/*.scss', ['sass']);
+})
